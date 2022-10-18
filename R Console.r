74194ba35aa495cd6e59a44a026b593c5d58bd2d
Tara <80751447+drunken-boat@users.noreply.github.com>
2022-10-18 22:03:58 +0800

    Update R Console.r

--- R Console.r
+++ R Console.r
@@ -7,21 +7,6 @@ R is free software and comes with ABSOLUTELY NO WARRANTY.
 You are welcome to redistribute it under certain conditions.
 Type 'license()' or 'licence()' for distribution details.
 
-  Natural language support but running in an English locale
-
-R is a collaborative project with many contributors.
-Type 'contributors()' for more information and
-'citation()' on how to cite R or R packages in publications.
-
-Type 'demo()' for some demos, 'help()' for on-line help, or
-'help.start()' for an HTML browser interface to help.
-Type 'q()' to quit R.
-
-[R.app GUI 1.78 (8075) x86_64-apple-darwin17.0]
-
-[Workspace restored from /Users/rainylty/.RData]
-[History restored from /Users/rainylty/.Rapp.history]
-
 > (1+7+4)/3
 [1] 4
 > x_bar <- 4
